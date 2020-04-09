385fc0adb47f999d336e7ac5a098d2bcd2ddb7b8
j721 <juliadvu@gmail.com>
2020-04-09 16:13:08 -0500

    Got input testing toBeInTheDocument working

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -2,22 +2,29 @@ import React from "react";
 import { render } from "@testing-library/react";
 import ContactForm from "./ContactForm";
 
-test ('inputs are visible',()=>{
+test("renders App without crashing", () => {
+    render(<ContactForm />);
+  });
+  
+
+test ('inputs are in the document',()=>{
     //Arrange-set up testing environment
-    const {getByLabelText} = render (<ContactForm/>);
+    const {getByText} = render (<ContactForm />);
 
     //Act. NONE.
 
     //Assert
-    const firstNameInput = getByLabelText(/firstName/i);
-    expect (firstNameInput).toBeVisible();
+    const firstNameInput = getByText(/first name/i);
+    expect (firstNameInput).toBeInTheDocument();
 
-    const lastNameInput = getByLabelText(/lastName/i);
-    expect (lastNameInput).toBeVisible();
+    const lastNameInput = getByText(/last name/i);
+    expect (lastNameInput).toBeInTheDocument();
 
-    const emailInput = getByLabelText(/email/i);
-    expect (emailInput).toBeVisible();
+    const emailInput = getByText(/email/i);
+    expect (emailInput).toBeInTheDocument();
 
-    const messageInput = getByLabelText(/message/i);
-    expect (messageInput).toBeVisible(); 
+    const messageInput = getByText(/message/i);
+    expect (messageInput).toBeInTheDocument(); 
 })
+
+test('')
