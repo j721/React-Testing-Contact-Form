73572d7e0fad80995b993e11934df74082fccd24
j721 <juliadvu@gmail.com>
2020-04-09 14:58:49 -0500

    Working on test for inputs

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -2,3 +2,22 @@ import React from "react";
 import { render } from "@testing-library/react";
 import ContactForm from "./ContactForm";
 
+test ('inputs are visible',()=>{
+    //Arrange-set up testing environment
+    const {getByLabelText} = render (<ContactForm/>);
+
+    //Act. NONE.
+
+    //Assert
+    const firstNameInput = getByLabelText(/firstName/i);
+    expect (firstNameInput).toBeVisible();
+
+    const lastNameInput = getByLabelText(/lastName/i);
+    expect (lastNameInput).toBeVisible();
+
+    const emailInput = getByLabelText(/email/i);
+    expect (emailInput).toBeVisible();
+
+    const messageInput = getByLabelText(/message/i);
+    expect (messageInput).toBeVisible(); 
+})
